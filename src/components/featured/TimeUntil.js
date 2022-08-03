1ee3564ed2ec6fd6836347f05697d584a06c3a84
Emmanuel Egomson <fegomson@gmail.com>
2022-08-03 15:46:35 -0500

    Finishing the countdown

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -1,7 +1,14 @@
-import { useState, useEffectm, useCallback, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Slide } from 'react-awesome-reveal';
 
 const TimeUntil = () => {
+  const [time, setTime] = useState({
+    days: '0',
+    hours: '0',
+    minutes: '0',
+    seconds: '0',
+  });
+
   const renderCountDown = (time, value) => {
     return (
       <div className="countdown_item">
@@ -11,13 +18,23 @@ const TimeUntil = () => {
     );
   };
 
-  const getTimeUntil = deadline => {
+  const getTimeUntil = useCallback(deadline => {
     const time = Date.parse(deadline) - Date.parse(new Date());
-    let day;
     if (time < 0) {
       console.log('Date passed!');
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
     }
-  };
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,15 +43,17 @@ const TimeUntil = () => {
     return () => clearInterval(interval);
   }, [getTimeUntil]);
 
+  const { days, hours, minutes, seconds } = time;
+
   return (
     <Slide direction="left" triggerOnce>
       <div className="countdown_wrapper">
         <div className="countdown_top">Event starts in...</div>
         <div className="countdown_bottom">
-          {renderCountDown(27, 'Days')}
-          {renderCountDown(3, 'hours')}
-          {renderCountDown(30, 'Minutes')}
-          {renderCountDown(40, 'Seconds')}
+          {renderCountDown(days, 'Days')}
+          {renderCountDown(hours, 'hours')}
+          {renderCountDown(minutes, 'Minutes')}
+          {renderCountDown(seconds, 'Seconds')}
         </div>
       </div>
     </Slide>
